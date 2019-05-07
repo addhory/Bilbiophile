59eddce0688645141bd64290efda878dabd9e128
xxridhohero11 <muhridhoanshory@gmail.com>
2019-05-07 23:26:37 +0800

    Add Splash Screen

---
+++ app/src/main/java/com/example/bilbiophile/splash/SplashScreen.java
@@ -0,0 +1,56 @@
+package com.example.bilbiophile.splash;
+
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.bilbiophile.MainActivity;
+import com.example.bilbiophile.R;
+
+
+public class SplashScreen extends AppCompatActivity {
+    ConstraintLayout constraintLayout;
+    ImageView logoImage;
+    TextView appName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        constraintLayout = findViewById(R.id.mConstraintSplash);
+        logoImage = findViewById(R.id.mImageLogo);
+        Animation mAnimation = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+        appName = findViewById(R.id.appName);
+        constraintLayout.startAnimation(mAnimation);
+        logoImage.startAnimation(mAnimation);
+        appName.startAnimation(mAnimation);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(5000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+
+    }
+}
+
